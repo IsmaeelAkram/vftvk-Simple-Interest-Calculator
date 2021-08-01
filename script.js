ee30bb5c55b94f25ca9f36311f0b2434a1fac693
Ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2021-08-01 03:34:14 -0400

    Final submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var amount = Number(document.getElementById("principal").value);
+    var rate = Number(document.getElementById("interest").value.replace("%", "")) / 100;
+    var currentYear = new Date().getFullYear();
+    var year = currentYear + Number(document.getElementById("years").value);
+
+    var answer = (amount*rate) * (year-currentYear);
+
+    var resultText = document.getElementById("result-text");
+    resultText.innerHTML = `If you deposit <span class="highlighted">${amount}</span>,<br/>
+                            at an interest rate of <span class="highlighted">${rate}%</span>.<br/>
+                            You will receive an amount of <span class="highlighted">${answer.toFixed(2)}</span>,<br/>
+                            in the year <span class="highlighted">${year}</span>\n<br/>`
 }
-        
+
+function updateRate(x){
+    var text = document.getElementById("rate-text");
+    text.innerText = x + "%";
+}
